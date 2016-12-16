1f0acb4466325bce38c61ae188cd657bdc64ca96
fromseto <wanglin44@gmail.com>
2016-12-16 11:30:25 -0500

    accepted

---
+++ leetcode/binary-tree-level-order-traversal_ii.py
@@ -0,0 +1,52 @@
+import collections
+import pdb
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution(object):
+    def findNodeofOneLevel(self,qp):
+        size = len(qp)
+        i = 0
+        for node in list(qp):
+            if i>= size: break
+            if node.left != None:
+                qp.append(node.left)
+            if node.right != None:
+                qp.append(node.right)
+            i = i+1
+            qp.popleft()
+        # print qp
+        return qp
+        
+    def levelOrderBottom(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        # q = Q.Queue()
+        q = collections.deque()
+        q.append(root)
+        
+        result = []
+        if root != None:
+            result = [[root.val]]
+            # while not q.empty():
+            while len(q) !=0:
+                q = self.findNodeofOneLevel(q)
+                if len(q) != 0:
+                    result.append([v.val for v in q])
+        result = result[::-1]
+        return result
+
+if __name__ == "__main__":
+    root = TreeNode(3)
+    root.left = TreeNode(9)
+    root.right = TreeNode(20)
+    root.right.left = TreeNode(15)
+    root.right.right = TreeNode(7)
+    result = Solution().levelOrderBottom(root)
+    print result
