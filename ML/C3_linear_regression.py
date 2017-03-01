a4ffcd4ef69987902704a04a6ab4baf28ac4fc11
fromseto <wanglin44@gmail.com>
2017-03-01 11:28:26 -0500

    simple linear regression

---
+++ ML/C3_linear_regression.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import sklearn.linear_model as skl_lm
+import numpy as np
+import matplotlib.pyplot as plt
+import statsmodels.formula.api as smf
+
+advertising_df = pd.read_csv("Data/Advertising.csv")
+# print df[:2]
+advertising_df.info()
+
+regr = skl_lm.LinearRegression()
+# X = scale(advertising.TV, with_mean=True, with_std=False).reshape(-1,1)
+X = advertising_df.TV.reshape(-1,1)
+y = advertising_df.Sales
+regr.fit(X,y)
+print(regr.intercept_)
+print(regr.coef_)
+
+est = smf.ols('Sales ~ TV', advertising_df).fit()
+print est.summary().tables[1]
+
